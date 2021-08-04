41c8cec25cca354b89e81fc2c3787f6df14efb8b
Edgar Alejandro Alba Perez <edgar.alba@utags.edu.mx>
2021-08-04 14:12:06 -0500

    eliminar controlPago cambio urlBack

--- src/enviroments/enviroments.url.jsx
+++ src/enviroments/enviroments.url.jsx
@@ -1,3 +1,3 @@
 
-export const Enviroments = { urlBack: 'http://localhost:3000', urlFront: 'http://localhost:3001' }
-//export const Enviroments = { urlBack: 'https://estacionamiento-web-back.herokuapp.com', urlFront: 'http://localhost:3001' }
+// export const Enviroments = { urlBack: 'http://localhost:3000', urlFront: 'http://localhost:3001' }
+export const Enviroments = { urlBack: 'https://estacionamiento-web-back.herokuapp.com', urlFront: 'http://localhost:3001' }
